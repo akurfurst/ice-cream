987dbca2afe29655205d7ab5a0bb0b866e5b5b6a
WillyxWonka <kalif.edu@gmail.com>
2026-01-29 14:49:31 -0800

    initial java commit

--- public/js/ice-cream.js
+++ public/js/ice-cream.js
@@ -1,1 +1,30 @@
-alert("Lorem-ipsum");
+const doc = document.getElementById("ice-cream-form");
+
+doc.onsubmit = ()=>{
+    clearErrors();
+    let isValid = true;
+
+    let name = document.getElementById("name");
+    let email = document.getElementById("email");
+
+
+    if(!name){
+        document.getElementById("err-name").style.display = "block";
+        isValid = false;
+    }
+
+    
+    if(!email){
+        document.getElementById("err-email").style.display = "block";
+        isValid = false;
+    }
+    return isValid;
+}
+
+
+function clearErrors(){
+    let errors = document.getElementsByClassName("err");
+    for(er of errors){
+        er.style.display = "none"
+    }
+}
