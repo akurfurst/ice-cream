a0b32980feac143bc45e10dc7727aee944ccb54b
akurfurst <219035144+akurfurst@users.noreply.github.com>
2026-01-29 14:29:37 -0800

    Organized files

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import express from 'express';
 const app = express();
 const PORT = 3000;
+
+app.use(express.static('public'));
+
+
 //define a default "route" ('/')
-app.get('/', (req, res) =>{
-   res.send('welcome') 
+app.get('/', (req, res) => {
+    res.sendFile(`${import.meta.dirname}/views/home.html`)
 });
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
     console.log(`server is running at http://localhost:${PORT}`);
 })
